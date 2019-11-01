d31db0be17c1d255162ae4126190603e4fdd7ec9
javadvhd <vahedi.r46@gmail.com>
2019-11-02 00:51:10 +0330

    fix : some console.log s removed

--- inline/src/logic/main/main.reducer.js
+++ inline/src/logic/main/main.reducer.js
@@ -3,9 +3,6 @@ import userReducer from '../user/user.reducer'
 import issueReducer from '../issue/issue.reducer'
 import { combineReducers } from 'redux'
 
-// console.log('userReducer ', userReducer)
-// console.log('issueReducer ', issueReducer)
-
 export default combineReducers({
   issue: issueReducer,
   user: userReducer,
